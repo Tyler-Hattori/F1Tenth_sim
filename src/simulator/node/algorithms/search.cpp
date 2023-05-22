8b0cd779f0fcdfcf08993d8646f28094153e3284
Tyler Hattori <77474488+Tyler-Hattori@users.noreply.github.com>
2023-05-21 18:24:58 -0700

    Update search.cpp

--- src/simulator/node/algorithms/search.cpp
+++ src/simulator/node/algorithms/search.cpp
@@ -125,8 +125,8 @@ public:
       tf::TransformListener listener;
       tf::StampedTransform pixy_to_map;
       ros::Time t = ros::Time(0);
-      listener.waitForTransform("map", "base_footprint", t, ros::Duration(30));
-      listener.lookupTransform("map", "base_footprint", t, pixy_to_map);
+      listener.waitForTransform("map", "base_link", t, ros::Duration(30));
+      listener.lookupTransform("map", "base_link", t, pixy_to_map);
       double th = (tf::getYaw(pixy_to_map.getRotation()));
       double car_x = pixy_to_map.getOrigin().getX(); // - 2*0.4318*cos(th);
       double car_y = pixy_to_map.getOrigin().getY(); // - 2*0.4318*sin(th);
